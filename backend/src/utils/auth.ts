0ab9c43b74a34b93c42a94c7686b22e657fdec88
Priyanshu Gusain <143826023+priyanshugusain2004@users.noreply.github.com>
2026-04-16 11:32:58 +0000

    some update

--- backend/src/utils/auth.ts
+++ backend/src/utils/auth.ts
@@ -28,8 +28,8 @@ export interface TokenPayload {
 }
 
 export const generateToken = (payload: TokenPayload): string => {
-  return jwt.sign(payload, config.jwt.secret, {
-    expiresIn: config.jwt.expiry,
+  return jwt.sign(payload, config.jwt.secret as string, {
+    expiresIn: config.jwt.expiry as jwt.SignOptions['expiresIn'],
   });
 };
 
